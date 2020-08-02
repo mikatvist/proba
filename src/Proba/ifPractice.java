6b4d8c6750be45c1d146983defec0895e123a388
Mikatvist <mikatvist@gmail.com>
2020-08-02 16:16:12 -0400

    IF

---
+++ src/Proba/ifPractice.java
@@ -0,0 +1,25 @@
+package Proba;
+
+public class ifPractice {
+    public static void main(String[] args) {
+
+        String schoolName = "Cybertek School";
+        System.out.println(schoolName);
+
+        String employeeName = "Vadym";
+        int employeeID = 9665;
+        String jobTitle = "SDET";
+        double salary = 12_000.50;
+        char gender = 'M';
+        boolean isFullTime = 10 > 9;
+
+        System.out.println("employee Name: " + employeeName + "\nemployee ID: " + employeeID + "\njob Title: " + jobTitle + "\nsalary: " + "$" + salary + "\ngander: " + gender + "\nis FullTime: " + isFullTime);
+        System.out.println("_______________OR_________________");
+        System.out.println("employeeID: " + employeeID);
+        System.out.println("jobTitle: " + jobTitle);
+        System.out.println("salary: " + "$" + salary);
+        System.out.println("gender: " + gender);
+        System.out.println("isFullTime: " + isFullTime);
+
+    }
+}
